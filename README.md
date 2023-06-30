b742ca208e00b776bd7eaa01289f59515723fc15
adxxtya <andronaut9@gmail.com>
2023-07-01 00:18:42 +0530

    Updating the README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Try it out [live](https://nyx-wallpaper.netlify.app)
+## Try it out [live](https://nyx-wallpaper.netlify.app)
+
+
+### Screenshots - 
+#1 ![MAIN](public/Readme/Main.png)
+#2 ![BROWSER](public/Readme/Browse.png)
+#3 ![GALLERY](public/Readme/Gallery.png)
+#4 ![DOWNLOAD](public/Readme/Download.png)
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,21 @@
 import Image from 'next/image'
 import React from 'react'
+import Link from 'next/link';
 
 function Navbar() {
     return (
         <nav
             className="w-[100%] h-[8dvh] bg-black text-white flex justify-between lg:h-[10vh] sticky top-0"
         >
-            <div
+            <a
+                href='/'
                 id="logo-sec"
                 className="w-[20%] flex items-center justify-center md:w-[25%] md:justify-evenly md:ml-4 lg:w-[10%] lg:ml-8 lg:justify-evenly"
             >
+
                 <Image src="/nyx-white.png" width="50" height="50" alt="NYX" />
                 <h1 className="text-5xl font-bold hidden md:block">NYX</h1>
-            </div>
+            </a>
             <div
                 id="navbar-items"
                 className="w-[60%] flex justify-center items-center lg:w-[40%]"
